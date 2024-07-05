08707c4c576976dc5ba99b316f9f8c1b9314a2b2
Luis Esquivia <luisdanielesq7@gmail.com>
2024-07-05 18:47:48 -0500

    feat (home page): add products grid

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,6 +1,10 @@
+import ProductGrid from "@/components/products/porduct-grid/ProductGrid";
 import Title from "@/components/ui/title/Title";
 import { titleFont } from "@/config/fonts";
-import Image from "next/image";
+import { initialData } from "@/seed/seed";
+
+
+const products = initialData.products; 
 
 export default function Home() {
   return (
@@ -9,6 +13,10 @@ export default function Home() {
       title="Tienda"
       subtitle="Todos los productos"
       className="mb-2"
+     />
+
+     <ProductGrid
+      products={products}
      />
     </>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,7 @@
 
+
+export * from './products/porduct-grid/ProductGrid';
+
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-fount/PageNotFound';
 export * from './ui/title/Title';
