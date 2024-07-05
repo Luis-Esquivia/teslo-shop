566886f148976eac09d6d935b9273759918efee5
Luis Esquivia <luisdanielesq7@gmail.com>
2024-07-04 19:20:52 -0500

    config (general): directory structure

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="">
      <h1>Hello word</h1>
-     <h1 className={ '${titleFont.className} font-bold' }>Hola mundo</h1>
+     <h1 className={ `${titleFont.className} font-bold` }>Hola mundo</h1>
     </main>
   );
 }
