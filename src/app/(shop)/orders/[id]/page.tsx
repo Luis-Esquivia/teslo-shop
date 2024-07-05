42aaca6f61d1cd6756565721be11fc69c9fb862a
Luis Esquivia <luisdanielesq7@gmail.com>
2024-07-04 20:20:30 -0500

    config (general): creating application routes

---
+++ src/app/(shop)/orders/[id]/page.tsx
@@ -0,0 +1,8 @@
+
+export default function() {
+  return (
+    <div>
+      <h1>Order abs</h1>
+    </div>
+  )
+}
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <div className="">
      <h1>Hello word</h1>
      <h1 className={ `${titleFont.className} font-bold` }>Hola mundo</h1>
-    </main>
+    </div>
   );
 }
