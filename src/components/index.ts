ea888a8ccd52acf864696c0718de19eddd2497f7
Luis Esquivia <luisdanielesq7@gmail.com>
2024-07-05 19:18:37 -0500

    feat (home page): add product assortment

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
 
 
 export * from './products/porduct-grid/ProductGrid';
+export * from './products/porduct-grid/ProductGridItem';
+
+
 
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-fount/PageNotFound';
